c0aa072e11c6972b9741980a242c7f16994843e6
EnkelediMema <56387565+EnkelediMema@users.noreply.github.com>
2020-05-28 11:25:53 +0200

    Add files via upload

---
+++ Path_E.Mema.cpp
@@ -0,0 +1,28 @@
+int short_path()
+{
+	int short_Distance();
+	int long_distance();
+while(1){
+	int check_sensors();
+	if(check_sensors='1')//All sensors and physical part are ok
+	{
+	
+       if(Water_mode_on='1')//Car can operate in water mode 
+         {
+         	printf("Water mode is active car is operating in  the short path ");
+		  int short_Distance();
+          }
+          else 
+          {
+		  printf("Water mode is not  active car is operating in the long path ");
+          int long_distance();
+          }
+      }
+      else
+      {
+      	printf("All sensors and physical part are not working properly ");
+      	Manual_Mode_On();//switching to manual mode 
+      	
+	  }
+          
+}
