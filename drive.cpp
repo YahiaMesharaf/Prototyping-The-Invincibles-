a79223c0e7e38e0966bd1240fac0b367f9bccf15
aluck101 <tundealuck@gmail.com>
2020-05-28 11:59:56 +0200

    DrivingCode

---
+++ drive.cpp
@@ -0,0 +1,21 @@
+void Drive() {
+	
+}
+
+bool checkRescueRobot() {
+	if(driveAbleOnWature)
+		Return True;
+	else return False;	
+}
+
+if (checkRescue == True) {
+	std::vector<RouteModel::Node> obstacle;
+	obstacle.emplace_back("Water");
+}
+
+float next_path = Astar().path;
+
+int main(){
+	Drive();
+}
+
