6bd89a0e89abc4014c4ad1d578e62473bd4fecc2
Tunde Aluko <tundealuck@gmail.com>
2020-06-04 11:55:00 +0200

    Updated the drive code

--- Codes/drive.cpp
+++ Codes/drive.cpp
@@ -3,22 +3,22 @@
 void Drive() {
 }
 
-vector<int> robot_position = Robot.getPosition();
-
-bool checkRescueRobot() {
-	if(driveAbleOnWature)
-		Return True;
-	else return False;	
-}
-
-if (checkRescue == True) {
-	std::vector<RouteModel::Node> obstacle;
-	obstacle.emplace_back("Water");
-}
-
-float next_path = Astar().path;
-
 int main(){
-	Drive();
+    vector<int> robot_position = robot.getPosition();
+
+    bool checkRescueRobot() {
+        if(driveAbleOnWature)
+            Return True;
+        else return False;
+    }
+
+    if (checkRescue == True) {
+        std::vector<RouteModel::Node> obstacle;
+        obstacle.emplace_back("Water");
+    }
+
+    float next_path = Astar().path;
+
+    Drive();
 }
 
