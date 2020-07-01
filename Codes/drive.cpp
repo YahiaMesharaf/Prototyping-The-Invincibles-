354ddadd98547d7178e540375ef9129571ed7886
Tunde Aluko <tundealuck@gmail.com>
2020-07-01 16:50:24 +0200

    Final

--- Codes/drive.cpp
+++ Codes/drive.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 
+#define L1 5
+#define L2 6
+#define R1 10
+#define R2 11
+
 void Drive() {
+}
+
+void backwardMovement() {
+  digitalWrite(L1, HIGH);
+  digitalWrite(L2, LOW);
+  digitalWrite(R1, HIGH);
+  digitalWrite(R2, LOW); 
+}
+
+void forwardMovement() {
+  digitalWrite(L1, LOW);
+  digitalWrite(L2, HIGH);
+  digitalWrite(R1, LOW);
+  digitalWrite(R2, HIGH); 
+}
+
+void moveRight() {
+  digitalWrite(L1, HIGH);
+  digitalWrite(L2, LOW);
+  digitalWrite(R1, LOW);
+  digitalWrite(R2, HIGH); 
+}
+
+void moveLeft() {
+  digitalWrite(L1, LOW);
+  digitalWrite(L2, HIGH);
+  digitalWrite(R1, HIGH);
+  digitalWrite(R2, LOW); 
+}
+
+void stopMotor() {
+  digitalWrite(L1, LOW);
+  digitalWrite(L2, LOW);
+  digitalWrite(R1, LOW);
+  digitalWrite(R2, LOW); 
 }
 
 int main(){
@@ -21,4 +61,6 @@ int main(){
 
     Drive();
 }
+
+
 
